20246673941d942e69143b350db4fa24624ed391
yunfei6688 <119996269+yunfei6688@users.noreply.github.com>
2023-09-28 16:39:08 +0800

    Add files via upload

---
+++ leijun-main/config/index.ts
@@ -0,0 +1,20 @@
+import type { AppInfo } from '@/types/app'
+export const APP_ID = `30c35245-2022-4429-8a49-0597a9a8727f`
+export const API_KEY = `app-ZALBmPU9GX4GDPL5yvPnRDuw`
+export const API_URL = `https://api.dify.ai/v1`
+export const APP_INFO: AppInfo = {
+  title: 'Chat APP',
+  description: '',
+  copyright: '',
+  privacy_policy: '',
+  default_language: 'zh-Hans',
+}
+
+export const isShowPrompt = false
+export const promptTemplate = 'I want you to act as a javascript console.'
+
+export const API_PREFIX = '/api'
+
+export const LOCALE_COOKIE_NAME = 'locale'
+
+export const DEFAULT_VALUE_MAX_LEN = 48
---
+++ leijun-main/i18n/lang/app.en.ts
@@ -0,0 +1,26 @@
+const translation = {
+  common: {
+    welcome: 'Welcome to use  yingteng  APP' +,
+    appUnavailable: 'yingteng App is unavailable',
+    appUnkonwError: ' yingteng App is unavailable',
+  },
+  chat: {
+    newChat: 'New chat',
+    newChatDefaultName: 'New conversation',
+    openingStatementTitle: 'Opening statement',
+    prompt: 'Prompt',
+    privatePromptConfigTitle: 'Conversation settings',
+    publicPromptConfigTitle: 'Initial Prompt',
+    configStatusDes: 'Before start, you can modify conversation settings',
+    configDisabled:
+      'Previous session settings have been used for this session.',
+    startChat: 'Start Chat',
+  },
+  errorMessage: {
+    valueOfVarRequired: 'Variables value can not be empty',
+    waitForResponse:
+      'Please wait for the response to the previous message to complete.',
+  }
+}
+
+export default translation
